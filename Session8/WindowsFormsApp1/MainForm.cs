a8203b4c155c09a98cb940007e7fdd8041496c6d
Giannis-Moi <85611864+Giannis-Moi@users.noreply.github.com>
2021-06-11 01:35:54 +0300

    Add files via upload

---
+++ Session8/WindowsFormsApp1/MainForm.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1 {
+
+    public partial class MainForm : Form {
+
+        List<Student> Students = new List<Student>();
+
+
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e) {
+
+            //MessageBox.Show("Hello World!");
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            //textBox1.Text = "Hello World!";
+
+            //checkBox1.Checked = true;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+
+            Student student = new Student();
+            // TODO: 1. ΝΑ ΠΕΡΑΣΩ ΤΟ STUDENT ΟΒJECT ΣΤΗΝ ΦΟΡΜΑ!
+
+            // TODO: OPEN FORM ...
+            StudentForm form = new StudentForm(student);
+            form.ShowDialog();
+
+
+            //TODO: WHEN FORM THE CLOSES ADD THE STUDENT TO A STUDENT LIST
+            if (!string.IsNullOrEmpty(student.FullName)) {
+
+                Students.Add(student);
+            }
+
+            Console.WriteLine(student.FullName + "Just show me this output");
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+        //Opens the Professor form 
+        private void button5_Click(object sender, EventArgs e) {
+            ProfessorForm professor = new ProfessorForm();
+
+            ProfessorForm form = new ProfessorForm(professor);
+            form.ShowDialog();
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            CourseForm courseForm = new CourseForm();
+
+            CourseForm form = new CourseForm(courseForm);
+            form.ShowDialog();
+        }
+    }
+}
---
+++ Session8/WindowsFormsApp1/ProfessorForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace WindowsFormsApp1 {
+    partial class ProfessorForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbFullName2 = new System.Windows.Forms.TextBox();
+            this.tbEmail2 = new System.Windows.Forms.TextBox();
+            this.tbTittle = new System.Windows.Forms.TextBox();
+            this.tbAdress2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(619, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(713, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 31);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Full Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tittle :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(148, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Email :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(148, 314);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Adress :";
+            // 
+            // tbFullName2
+            // 
+            this.tbFullName2.Location = new System.Drawing.Point(309, 109);
+            this.tbFullName2.Name = "tbFullName2";
+            this.tbFullName2.Size = new System.Drawing.Size(152, 22);
+            this.tbFullName2.TabIndex = 6;
+            // 
+            // tbEmail2
+            // 
+            this.tbEmail2.Location = new System.Drawing.Point(309, 236);
+            this.tbEmail2.Name = "tbEmail2";
+            this.tbEmail2.Size = new System.Drawing.Size(152, 22);
+            this.tbEmail2.TabIndex = 8;
+            // 
+            // tbTittle
+            // 
+            this.tbTittle.Location = new System.Drawing.Point(309, 169);
+            this.tbTittle.Name = "tbTittle";
+            this.tbTittle.Size = new System.Drawing.Size(152, 22);
+            this.tbTittle.TabIndex = 9;
+            // 
+            // tbAdress2
+            // 
+            this.tbAdress2.Location = new System.Drawing.Point(309, 309);
+            this.tbAdress2.Name = "tbAdress2";
+            this.tbAdress2.Size = new System.Drawing.Size(152, 22);
+            this.tbAdress2.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label5.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label5.Location = new System.Drawing.Point(252, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(257, 32);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Add New Professor";
+            // 
+            // ProfessorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbAdress2);
+            this.Controls.Add(this.tbTittle);
+            this.Controls.Add(this.tbEmail2);
+            this.Controls.Add(this.tbFullName2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "ProfessorForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbFullName2;
+        private System.Windows.Forms.TextBox tbEmail2;
+        private System.Windows.Forms.TextBox tbTittle;
+        private System.Windows.Forms.TextBox tbAdress2;
+        private System.Windows.Forms.Label label5;
+    }
+}
